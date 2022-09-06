613e166d5d0163c7a2d39c2e5425d56c080d4a5b
Vitor Alves <vitorcarneiro778@gmail.com>
2022-09-06 11:41:22 -0300

    Outros componentes

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+
+        <img src={logo} alt="DSMeta"></img>
+          <h1>DSMeta</h1>
+          <p>Desenvolvido por
+            <a href="https://github.com/cronodrak">@cronovitor777</a>
+          </p>
+
+      </div>
+    </header>
+  )
+
+}
+
+export default Header
